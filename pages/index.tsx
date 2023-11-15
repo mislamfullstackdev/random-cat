c9a45451a8836bd1193ae105c282036b942a32b7
mislamfullstackdev <mislam.pasona@gmail.com>
2023-11-15 14:03:30 +0900

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,44 @@
+import { NextPage} from 'next'
+import Image from 'next/image';
+// import { type } from 'os';
+import { useEffect, useState } from 'react';
+
+const IndexPage: NextPage = () => {
+    const [imageUrl, setImageUrl] = useState("");
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        fetchImage().then((newImage) => {
+            setImageUrl(newImage.url)
+            setLoading(false)
+        });
+    }, []);
+    const handleClick =  async () =>{
+        setLoading(true);
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url)
+        setLoading(false)
+    }
+    return (
+        <div>
+            <button onClick={handleClick}>Click Me To Get New Cat</button>
+            <div>{loading || <img src={imageUrl} style={{ width: '60%', height: '45%' }} />}</div>
+        </div>
+    )
+};
+
+export default IndexPage;
+
+type Image = {
+    url: string;
+}
+
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images[0];
+};
+
+// call the fetchImage function
+
+fetchImage();
